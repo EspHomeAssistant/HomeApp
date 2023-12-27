582f2641cd7c292e0c680cfe6c86ae4f70f8c59e
Patryk Kaczmarek <patryk.kaczmarek996@gmail.com>
2023-12-27 13:47:20 +0100

    Add logger

--- src/api/mqtt/mqtt.hpp
+++ src/api/mqtt/mqtt.hpp
@@ -1,11 +1,23 @@
 #include <string>
+#include <memory>
+
+namespace spdlog 
+{
+    class logger;
+}
 
 class Mqtt {
 public:
-    Mqtt(const std::string& ipAddr);
+    Mqtt(const std::string& broker, std::shared_ptr<spdlog::logger> logger);
     ~Mqtt();
     Mqtt(const Mqtt&) = delete;
     Mqtt(Mqtt&&) = delete;
     Mqtt& operator=(const Mqtt&) = delete;
     Mqtt& operator=(Mqtt&&) = delete;
+
+    std::shared_ptr<spdlog::logger> getLogger();
+
+private:
+    const std::string broker_;
+    std::shared_ptr<spdlog::logger> logger_;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+
 #include "api/mqtt/mqtt.hpp"
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 int main() {
-    Mqtt("");
+    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/basic-log.txt");
+    auto logger = std::make_shared<spdlog::logger>("main", file_sink);
+    auto mqttLogger = std::make_shared<spdlog::logger>("mqtt", file_sink);
+    logger->info("Hello world");
+
+    auto mqtt = Mqtt{"127.0.0.1", mqttLogger};
+    std::this_thread::sleep_for(std::chrono::seconds(5));
     std::cout << "HelloWorld\n";
     return 0;
 }
